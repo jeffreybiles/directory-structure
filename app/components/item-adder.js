2fa3223fece566b04b2af8cb5b73a4131e61cf80
jeffreybiles <bilesjeffrey@gmail.com>
2016-09-02 15:51:16 -0500

    Add folders and files

---
+++ app/components/item-adder.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    startItemNaming(){
+      this.set('isEditing', true)
+    },
+    save(){
+      this.set('isEditing', false)
+      
+      let parent = this.get('parent');
+      let itemType = this.get('itemType');
+
+      let item = this.get('store').createRecord(itemType, {
+        name: this.get('name')
+      })
+      if(itemType == 'file'){
+        item.set('folder', parent);
+      } else {
+        item.set('parent', parent);
+      }
+      item.save();
+      parent.save();
+    }
+  }
+});
--- app/controllers/folders.js
+++ app/controllers/folders.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  rootFolders: Ember.computed.filterBy('model', 'parent.content', null)
+  rootFolders: Ember.computed.filterBy('model', 'parent.content', null),
+  actions: {
+    saveItem(itemType, name, parent){
+      let item = this.store.createRecord(itemType, {
+        name: name
+      })
+      if(itemType == 'file'){
+        item.set('folder', parent);
+      } else {
+        item.set('parent', parent);
+      }
+      item.save();
+      parent.save();
+    }
+  }
 });
