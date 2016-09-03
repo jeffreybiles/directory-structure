24225b520ed7f429503e7fc66f1da9bc5602d479
jeffreybiles <bilesjeffrey@gmail.com>
2016-09-03 03:29:38 -0500

    filesize and folder size

--- app/models/file.js
+++ app/models/file.js
@@ -2,5 +2,14 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  folder: DS.belongsTo()
+  folder: DS.belongsTo(),
+  size: DS.attr('number'),
+
+  displaySize: Ember.computed('size', function(){
+    if(this.get('size')){
+      return `${this.get('size')}kb`;
+    } else {
+      return 'unknown size'
+    }
+  })
 });
--- app/models/folder.js
+++ app/models/folder.js
@@ -4,5 +4,21 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   files: DS.hasMany(),
   parent: DS.belongsTo('folder', {inverse: 'folders'}),
-  folders: DS.hasMany('folder', {inverse: 'parent'})
+  folders: DS.hasMany('folder', {inverse: 'parent'}),
+
+  folderSizes: Ember.computed('folders.@each.size', function(){
+    return this.get('folders').map((folder)=>{
+      return parseInt(folder.get('size') || 0);
+    })
+  }),
+  foldersSize: Ember.computed.sum('folderSizes'),
+  fileSizes: Ember.computed('files.@each.size', function(){
+    return this.get('files').map((file)=>{
+      return parseInt(file.get('size') || 0);
+    })
+  }),
+  filesSize: Ember.computed.sum('fileSizes'),
+  size: Ember.computed('foldersSize', 'filesSize', function(){
+    return parseInt(this.get('foldersSize')) + parseInt(this.get('filesSize'));
+  })
 });
